261359a7042a876c6fc1a4ca475bf2b9078d5ab0
Mohamed Mohy <96388937+Mmohyy@users.noreply.github.com>
2021-12-25 01:22:25 +0200

    Create problem8-sockmerchant.c

---
+++ Task3/problem8-sockmerchant.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     int n,i,j,count=0,max=0,res=0;
+  printf("Number:");
+  scanf("%d",&n);
+  int arr[n];
+  for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+
+    for(i=0;i<n;i++)
+    {
+       for(j=i+1;j<n;j++)
+         {
+           if(arr[i]==arr[j])
+            count++;
+         }
+      res=res+(count/2);
+    }
+  printf("%d",res/10);
+}
