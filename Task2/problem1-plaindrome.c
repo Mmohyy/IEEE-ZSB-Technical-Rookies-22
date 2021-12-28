6116f871f260f982480036b15ab9d4111002bfd1
Mohamed Mohy <96388937+Mmohyy@users.noreply.github.com>
2021-12-28 03:02:50 +0200

    Update problem1-plaindrome.c

--- Task2/problem1-plaindrome.c
+++ Task2/problem1-plaindrome.c
@@ -6,7 +6,7 @@ int plaind (char a[]);
  {
   char word[100];
     printf("please enter the word:");
-    scanf("%s",&word);
+    scanf("%s",word);
 
    if(plaind(word)==1) printf("\n%s is plaindrome",word);
    else printf("\n%s isn't plaindrome",word);
@@ -18,9 +18,10 @@ int plaind (char a[])
 int b=1,i;
   for(i=0;i<strlen(a)/2;i++)
   {
-      if(a[i]!= a[strlen(a)-1-i])
+      if(a[i]!= a[strlen(a)-1-i]){
         b=0;
         break;
+      }
   }
   return(b);
 }
